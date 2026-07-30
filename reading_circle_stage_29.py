402fddd6e78c8788d84cc6b2d204fb8eb31c2812
Monica <taproot_murmurs_2c@icloud.com>
2026-07-30 22:16:46 +0200

    Stage 29: Добавь конфигурацию приложения через словарь настроек

---
+++ reading_circle_stage_29.py
@@ -0,0 +1,9 @@
+# === Stage 29: Добавь конфигурацию приложения через словарь настроек ===
+# Project: ReadingCircle
+APP_CONFIG = {
+    "app_name": "ReadingCircle",
+    "version": 29,
+    "max_questions_per_book": 10,
+    "meeting_frequency_days": 7,
+    "default_note_visibility": "public",
+}
